def3d632a2b5bb90092891119b9223d3826838ed
gui11epio <guilherme.laiber@gmail.com>
2025-06-02 12:09:06 -0300

    Endereco liberado

--- src/main/java/br/com/fiap/geoalerta/GeoAlertaApplication.java
+++ src/main/java/br/com/fiap/geoalerta/GeoAlertaApplication.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "GeoAlerta",
                 version = "1.0",
-                description = "Documentação da API para o aviso de deslizamento"
+                description = "Documentação da API para o alerta de deslizamento"
         )
 )
 
